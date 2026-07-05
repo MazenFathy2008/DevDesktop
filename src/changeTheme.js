42e6785ed15ddde700e70a950e261dd7bc6f5c5f
mazenDev <mazenfathy841@gmail.com>
2026-07-06 02:01:16 +0300

    starting with the main code, creating header, adding toggle theme option

---
+++ src/changeTheme.js
@@ -0,0 +1,7 @@
+const divCheck = document.querySelector(".toggleThemeJs")
+const inputCheckbox = document.querySelector(".inputCheckBoxJs")
+const body = document.querySelector("body")
+divCheck.addEventListener("click",()=>{
+  inputCheckbox.checked = !inputCheckbox.checked;
+  body.classList.toggle("dark",inputCheckbox.checked)
+})
