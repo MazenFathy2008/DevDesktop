a7df25eb3c5b7a17d0b9198bc4a1b12e0ff4e808
mazenDev <mazenfathy841@gmail.com>
2026-07-06 13:09:53 +0300

    Adding and styling header navBar

--- src/changeTheme.js
+++ src/changeTheme.js
@@ -1,7 +1,9 @@
-const divCheck = document.querySelector(".toggleThemeJs")
-const inputCheckbox = document.querySelector(".inputCheckBoxJs")
-const body = document.querySelector("body")
-divCheck.addEventListener("click",()=>{
-  inputCheckbox.checked = !inputCheckbox.checked;
-  body.classList.toggle("dark",inputCheckbox.checked)
-})
+export const changeTheme = () => {
+  const divCheck = document.querySelector(".toggleThemeJs");
+  const inputCheckbox = document.querySelector(".inputCheckBoxJs");
+  const body = document.querySelector("body");
+  divCheck.addEventListener("click", () => {
+    inputCheckbox.checked = !inputCheckbox.checked;
+    body.classList.toggle("dark", inputCheckbox.checked);
+  });
+};
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import{changeTheme} from"./changeTheme.js"
+const main=()=>{
+  changeTheme()
+}
+main()
