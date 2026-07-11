337fe09efe43a5b6729994e03125ee17ec1669eb
mazenDev <mazenfathy841@gmail.com>
2026-07-11 17:25:49 +0300

    saving the current theme on localstorage

--- src/changeTheme.js
+++ src/changeTheme.js
@@ -2,8 +2,18 @@ export const changeTheme = () => {
   const divCheck = document.querySelector(".toggleThemeJs");
   const inputCheckbox = document.querySelector(".inputCheckBoxJs");
   const body = document.querySelector("body");
+  console.log(localStorage.getItem("mode"));
+  const savedMode = localStorage.getItem("mode");
+
+  if (savedMode !== null) {
+    inputCheckbox.checked = JSON.parse(savedMode);
+    body.classList.toggle("dark", inputCheckbox.checked);
+  } else {
+    inputCheckbox.checked = true;
+  }
   divCheck.addEventListener("click", () => {
     inputCheckbox.checked = !inputCheckbox.checked;
+    localStorage.setItem("mode", JSON.stringify(inputCheckbox.checked));
     body.classList.toggle("dark", inputCheckbox.checked);
   });
 };
